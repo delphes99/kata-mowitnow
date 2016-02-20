2744aee742becacd1041c7bc6ef57585f0dbb784
Delphes <Delphes@192.168.1.15>
2016-02-20 22:30:02 +0100

    Initialisation

---
+++ mowitnow/src/main/java/com/delphes99/mowitnow/App.java
@@ -0,0 +1,13 @@
+package com.delphes99.mowitnow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
